8f17e13f6db87b70b8696c047237a4e051d32ac1
Glitch (glitch-hello-website) <none>
2021-04-25 20:14:07 +0000

    🏬🏇 Checkpoint ./index.html:29511417/2222 ./script.js:29511417/963 ./.glitch-assets:29511417/1476

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+//	Global Variables
+var myStage;
+var hero;
+//	1. Add a new variable named bg
 
-// prints a message in the browser's dev tools console
-console.log("Hello 🌎");
+function init(){
+	myStage = new createjs.Stage(document.getElementById("myCanvas"));
+	createjs.Ticker._setFPS(60);
+	createjs.Ticker.addEventListener('tick', gameLoop);
+	initGame();	
+}
+
+function initGame(){
+//	2. Assign the bg variable a new createjs.Bitmap() and...
+//	3. Add the bg to the Stage using the .addChild() method
+	hero = new createjs.Bitmap("images/hero.png");
+	myStage.addChild(hero);
+//	4. Set the hero's registration point to be in the center of the image (vs upper left)
+//	hero.regX = 64;
+// 	hero.regY = 64;
+//	5. Adjust the hero's .x and .y position on the stage...
+//	hero.x = 512;
+}
+
+function gameLoop(){
+	myStage.update();
+}
