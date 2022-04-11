fe08e4ddcc1b70a83bc1e89a2af93cf8b6ec9beb
Glitch (glitch-hello-website) <none>
2022-04-11 18:05:59 +0000

    🍵⛹ Checkpoint ./script.js:29511417/765

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 /* global createjs */
 //	Global Variables
 
+// for loops
 
+// for(var i = 0; i < 5; i++){
+//   document.write("The number is "+ i);
+//   document.write("<br>");
+// }
 
+// for(var i = 0; i < 100; i++){
+//   document.write("All work and no play...");
+//   document.write("<br>");
+// }
 
+// for(var i = 0; i < 100; i++){
+//   document.write(i * 64);
+//   document.write("<br>");
+// }
 
 
 
@@ -21,7 +34,7 @@
 
 
 var myStage;
-
+var hero;
 
 function init(){
 	myStage = new createjs.Stage(document.getElementById("myCanvas"));
@@ -31,7 +44,11 @@ function init(){
 }
 
 function initGame(){
-
+// display 8 heros in a line
+  for(var i = 0; i < 8; i++){
+    // display a hero
+    hero = new createjs.bitmap("")
+  }
 }
 
 function gameLoop(){
