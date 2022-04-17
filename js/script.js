8b8d5433d6be88fb3db78d8102468e86dd7d0d5e
Glitch (glitch-hello-website) <none>
2022-04-17 03:46:07 +0000

    ⚽️🎈 Checkpoint ./js/script.js:29511417/3368

--- js/script.js
+++ js/script.js
@@ -3,266 +3,296 @@
   /* global keyMonkey */
   /* global collisionGnome */
 
-	var myStage;
-	var stageWidth;
-	var stageHeight;
-	var myFrameRate = 24;
-	var character, background, scoreDisplay, theEnd;
-	var rotationSpeed = 3;
-	var targets = [];
-	var score = 0;
-	var a,b,c,xpos,ypos;
-	var endSoundPlayed = false;
-	
-	
-	function preload(){
-		var queue = new createjs.LoadQueue();
-		createjs.Sound.alternateExtensions = ["mp3"];
-		queue.installPlugin(createjs.Sound);
-		queue.addEventListener("complete", init);
-		queue.loadManifest([
-			{id:"backgroundSound", src:"sounds/slowbackground.mp3"},
-			{id:"eatSound1", src:"sounds/eat1.mp3", data:2},
-			{id:"eatSound2", src:"sounds/eat2.mp3", data:2},
-			{id:"eatSound3", src:"sounds/eat3.mp3", data:2},
-			{id:"eatSound4", src:"sounds/eat4.mp3", data:2},
-			{id:"eatSound5", src:"sounds/eat5.mp3", data:2},
-			{id:"endSound", src:"sounds/endSound.mp3", data:2},
-			{id:"edgeSound", src:"sounds/edgeSound.mp3", data:2}
-			]);
-	}
-	
-	
-	// ************ INITIALIZATION - Happens only once ****************
-	function init(){
-		myStage = new createjs.Stage(document.getElementById("myCanvas"));
-			stageWidth = myStage.canvas.width;
-			stageHeight = myStage.canvas.height;
-			
-		// generateStars(500);
-		// generateTargets(10); 
-		
-		background = new createjs.Bitmap("images/bg.png");
-		myStage.addChild(background);
-		
-		character = new createjs.Bitmap("images/box_yellow.png");
-		character.regX = character.regY = 45;
-		character.x = stageWidth / 2;
-		character.y = stageHeight - 64;
-		character.speed = 10;
-		myStage.addChild(character);
-		
-		collisionGnome.addCollider(character, 1.0);
-		//collisionGnome.setDebug(true);
-		
-		scoreDisplay = new createjs.Text("SCORE: " + score, "48px Courier");
-		scoreDisplay.x = stageWidth - 50;
-		scoreDisplay.y = stageHeight - 64;
-		scoreDisplay.textAlign = "right";
-		scoreDisplay.textBaseline = "middle";
-		myStage.addChild(scoreDisplay);
-		
-		theEnd = new createjs.Text("THE END", "144px Courier");
-		theEnd.x = stageWidth / 2;
-		theEnd.y = stageHeight / 2;
-		theEnd.textAlign = "center";
-		theEnd.textBaseline = "middle";
-		theEnd.alpha = 0.;
-		myStage.addChild(theEnd);
-		
-		createjs.Sound.play("backgroundSound");
-		
-		createjs.Ticker.addEventListener('tick', gameLoop);
-		createjs.Ticker.setFPS(myFrameRate);
-	}
+var myStage;
+var stageWidth;
+var stageHeight;
+var myFrameRate = 24;
+var character, background, scoreDisplay, theEnd;
+var rotationSpeed = 3;
 
-	// ***************** THE MAIN LOOP ******************
-	function gameLoop(evt){
-		// put code in here that will change every 'tick'
-		handleKeyInput();
-		handleCollisions();
-		handlePerimeter();
-		endCheck();
-		myStage.update();
-	}
-	
-	
-	// ************* Various self-defined functions *************
-	function generateStars(num){
-		// create any number of stars, random location and size
-		for(var i=0;i<num;i++){
-			var star = new createjs.Bitmap("images/star.png");
-			star.scaleX = star.scaleY = Math.random();
-	        star.rotation = 360 * Math.random();
-			star.x = Math.random() * stageWidth;
-			star.y = Math.random() * stageHeight;
-			star.alpha = 0.5
-			myStage.addChild(star);
-		}
-	}
+var walls = [];
+var targets = [];
+var enemies = [];
 
-  function level1(){
-    // create walls
+var score = 0;
+var a,b,c,xpos,ypos;
+var endSoundPlayed = false;
 
-    
-    // Design the level. x = wall, t = target, e = enemy.
-    var level1 = [
-        "                 ",
-        "                 ",
-        "                 ",
-        "     t           ",
-        "   e             ",
-        "   xxxxxxx       ",
-        "                 ",
-        "                t",
-        "             xxxx",
-        "                 ",
-        "    t   e    e   ",
-        "xxxxxxxxxxxxxxxxx"
-    ];  
-    
-    // create targets
-    
-    // reposition character
-    character.x = stageWidth / 2;
-		character.y = stageHeight - 64;
+
+
+function preload(){
+  var queue = new createjs.LoadQueue();
+  createjs.Sound.alternateExtensions = ["mp3"];
+  queue.installPlugin(createjs.Sound);
+  queue.addEventListener("complete", init);
+  queue.loadManifest([
+    {id:"backgroundSound", src:"sounds/slowbackground.mp3"},
+    {id:"eatSound1", src:"sounds/eat1.mp3", data:2},
+    {id:"eatSound2", src:"sounds/eat2.mp3", data:2},
+    {id:"eatSound3", src:"sounds/eat3.mp3", data:2},
+    {id:"eatSound4", src:"sounds/eat4.mp3", data:2},
+    {id:"eatSound5", src:"sounds/eat5.mp3", data:2},
+    {id:"endSound", src:"sounds/endSound.mp3", data:2},
+    {id:"edgeSound", src:"sounds/edgeSound.mp3", data:2}
+    ]);
+}
+
+
+// ************ INITIALIZATION - Happens only once ****************
+function init(){
+  myStage = new createjs.Stage(document.getElementById("myCanvas"));
+    stageWidth = myStage.canvas.width;
+    stageHeight = myStage.canvas.height;
+
+  // generateStars(500);
+  // generateTargets(10); 
+
+  background = new createjs.Bitmap("images/bg.png");
+  myStage.addChild(background);
+
+  character = new createjs.Bitmap("images/box_yellow.png");
+  character.regX = character.regY = 45;
+  character.x = stageWidth / 2;
+  character.y = stageHeight - 64;
+  character.speed = 10;
+  myStage.addChild(character);
+
+  collisionGnome.addCollider(character, 1.0);
+  //collisionGnome.setDebug(true);
+
+  scoreDisplay = new createjs.Text("SCORE: " + score, "48px Courier");
+  scoreDisplay.x = stageWidth - 50;
+  scoreDisplay.y = stageHeight - 64;
+  scoreDisplay.textAlign = "right";
+  scoreDisplay.textBaseline = "middle";
+  myStage.addChild(scoreDisplay);
+
+  theEnd = new createjs.Text("THE END", "144px Courier");
+  theEnd.x = stageWidth / 2;
+  theEnd.y = stageHeight / 2;
+  theEnd.textAlign = "center";
+  theEnd.textBaseline = "middle";
+  theEnd.alpha = 0.;
+  myStage.addChild(theEnd);
+
+  createjs.Sound.play("backgroundSound");
+
+  createjs.Ticker.addEventListener('tick', gameLoop);
+  createjs.Ticker._setFPS(myFrameRate);
+  
+  loadLevel(level1);
+}
+
+// ***************** THE MAIN LOOP ******************
+function gameLoop(evt){
+  // put code in here that will change every 'tick'
+  handleKeyInput();
+  handleCollisions();
+  handlePerimeter();
+  endCheck();
+  myStage.update();
+}
+
+
+// ************* Various self-defined functions *************
+function generateStars(num){
+  // create any number of stars, random location and size
+  for(var i=0;i<num;i++){
+    var star = new createjs.Bitmap("images/star.png");
+    star.scaleX = star.scaleY = Math.random();
+        star.rotation = 360 * Math.random();
+    star.x = Math.random() * stageWidth;
+    star.y = Math.random() * stageHeight;
+    star.alpha = 0.5
+    myStage.addChild(star);
   }
+}
+
+function level1(){
+
+  // Design the level. x = wall, t = target, e = enemy.
+  var level1 = [
+      "                 ",
+      "                 ",
+      "                 ",
+      "     t           ",
+      "   e             ",
+      "   xxxxxxx       ",
+      "                 ",
+      "                t",
+      "             xxxx",
+      "                 ",
+      "    t   e    e   ",
+      "xxxxxxxxxxxxxxxxx"
+  ];  
+
+  // create targets
+
+  // reposition character
+  character.x = stageWidth / 2;
+  character.y = stageHeight - 64;
+}
 
 
 // Create the level from the level data grid in create
-  function loadLevel (level) {
-      for (var i = 0; i < level.length; i = i + 1) {
-          for (var j = 0; j < level[i].length; j = j + 1) {
-              if (level[i][j] === "x") { 
-                  // Create a wall and add it to the 'platform' group
-                  var platform = game.add.sprite(0 + 32 * j, 0 + 32 * i, "platform");
-                  platform.body.immovable = true;
-                  platforms.add(platform);
+function loadLevel (level) {
+  walls = [];
+  targets = [];
+  enemies = [];
+  
+    for (var i = 0; i < level.length; i = i + 1) {
+        for (var j = 0; j < level[i].length; j = j + 1) {
+            if (level[i][j] === "x") { 
+                // Create a wall and add it to the 'walls' group
+                var wall = new createjs.Bitmap("iamges/box_yellow.png");
+                wall.x = 64*j;
+                wall.y = 64*i;
+                wall.addCollider();
+                walls.push(wall);
 
-              } else if (level[i][j] === "o") { 
-                  // Create a coin and add it to the 'coins' group
-                  var coin = game.add.sprite(0 + 32 * j, 0 + 32 * i, "coin");
-                  coins.add(coin);
+            } else if (level[i][j] === "t") { 
+                // Create a coin and add it to the 'targets' group
+                var target = new createjs.Bitmap("iamges/box_green.png");
+                target.x = 64*j;
+                target.y = 64*i;
+                target.addCollider();
+                targets.push(target);
 
-              } else if (level[i][j] === "h") { 
-                  // Create a enemy and add it to the 'enemies' group
-                  var enemy = game.add.sprite(0 + 32 * j, 0 + 32 * i, "enemy");
-                  enemies.add(enemy);
+            } else if (level[i][j] === "h") { 
+                // Create a enemy and add it to the 'enemies' group
+                var enemy = new createjs.Bitmap("iamges/box_orange.png");
+                enemy.x = 64*j;
+                enemy.y = 64*i;
+                enemy.addCollider();
+                enemies.push(enemy);
 
-              }
-          }
-      }
+            }
+        }
+    }
+
+  display(walls);
+  display(targets);
+  display(enemies);
+  display(character);
+
+}
+
+function display(object){
+  
+  for(var i=0; i<object.length; i++){
+    myStage.addChild(object[i]);
   }
-	
-	function generateTargets(num){
-		// create any number of targets
-		for(var i=0;i<num;i++){
-			var target = new createjs.Bitmap("images/target.png");
-			target.scaleX = target.scaleY = Math.min(Math.max(Math.random(), 0.3), 1);
-			target.rotation = 360 * Math.random();
-			
-			// call this function until we get a locations within bounds of circle
-			while(targetLocator());
-			
-			target.x = xpos;
-			target.y = ypos;
-			
-			targets[i]= target;
-			collisionGnome.addCollider(targets[i], 0.5);
-			myStage.addChild(target);
-		}	
-	}
-	
-	function targetLocator(){
-	    // generate locations within bounds of circle
-		xpos = Math.random() * stageWidth;
-		ypos = Math.random() * stageHeight;
-		
-		a = (stageWidth/2) - xpos;
-		b = (stageHeight/2) - ypos;
-		c = Math.sqrt( a*a + b*b );
-		
-		if (c > 450) { return true;}
-		else { return false;}
-	}
-	
-	function handleKeyInput(){
-		// allow key strokes to control movement
-		if(keyMonkey["w"] || keyMonkey["up"]) 		{ character.y -= character.speed; }
-		if(keyMonkey["a"] || keyMonkey["left"]) 	{ character.x -= character.speed; }
-		if(keyMonkey["s"] || keyMonkey["down"]) 	{ character.y += character.speed; }
-		if(keyMonkey["d"] || keyMonkey["right"]) 	{ character.x += character.speed; }
-		
-	}
-	
-	function handleCollisions(){
-		// check to see if there are any collisions with any of the targets
-		for(var i=0;i<targets.length;i++){
-			if(character.collidesWith(targets[i])){
-				// move target out of view and remove it from the array
-				//targets[i].x = 0;
-				//targets[i].y = 0;
-				myStage.removeChild(targets[i]);
-				targets.splice(i, 1);
-				score++;
-				scoreDisplay.text = score;
-				playEatSounds();
-			}
-		}
-	}
-	
-	
-	function handlePerimeter(){
-		// check distance from center of the circle, if too big reset to center
-		var x = (stageWidth/2) - character.x;
-		var y = (stageHeight/2) - character.y;
-		var z = Math.sqrt( x*x + y*y );
-		if (z > 415){
-			character.x = stageWidth/2;
-			character.y = stageHeight/2;
-			createjs.Sound.play("edgeSound");
-			
-			// set score to zero unless end of game
-			if(theEnd.arrived != true){
-				score = 0;
-				scoreDisplay.text = score;
-			}
-		}
-	}
-	
-	
-	
-	function playEatSounds(){  
-			var i = Math.floor((Math.random()*5));
-			var x; 
-			switch(i){
-				case 0:
-					x="eatSound1";
-					break;
-				case 1:
-					x="eatSound2";
-					break;
-				case 2:
-					x="eatSound3";
-					break;
-				case 3:
-					x="eatSound4";
-					break;
-				case 4:
-					x="eatSound5";
-					break;
-			}
-		createjs.Sound.play(x);  // Play the sound that corresponds to the random number!
-	}
-	
-	function endCheck(){
-		// if all targets are gone, display end text and don't allow score to reset
-		if(targets.length == 0){
-			theEnd.arrived = true;
-			theEnd.alpha = 1.0;
-		}
-		if (theEnd.arrived == true && endSoundPlayed == false){
-			createjs.Sound.play("endSound");
-			endSoundPlayed = true;
-		}
-	}
+}
+
+function generateTargets(num){
+  // create any number of targets
+  for(var i=0;i<num;i++){
+    var target = new createjs.Bitmap("images/target.png");
+    target.scaleX = target.scaleY = Math.min(Math.max(Math.random(), 0.3), 1);
+    target.rotation = 360 * Math.random();
+
+    // call this function until we get a locations within bounds of circle
+    while(targetLocator());
+
+    target.x = xpos;
+    target.y = ypos;
+
+    targets[i]= target;
+    collisionGnome.addCollider(targets[i], 0.5);
+    myStage.addChild(target);
+  }	
+}
+
+function targetLocator(){
+    // generate locations within bounds of circle
+  xpos = Math.random() * stageWidth;
+  ypos = Math.random() * stageHeight;
+
+  a = (stageWidth/2) - xpos;
+  b = (stageHeight/2) - ypos;
+  c = Math.sqrt( a*a + b*b );
+
+  if (c > 450) { return true;}
+  else { return false;}
+}
+
+function handleKeyInput(){
+  // allow key strokes to control movement
+  if(keyMonkey["w"] || keyMonkey["up"]) 		{ character.y -= character.speed; }
+  if(keyMonkey["a"] || keyMonkey["left"]) 	{ character.x -= character.speed; }
+  if(keyMonkey["s"] || keyMonkey["down"]) 	{ character.y += character.speed; }
+  if(keyMonkey["d"] || keyMonkey["right"]) 	{ character.x += character.speed; }
+
+}
+
+function handleCollisions(){
+  // check to see if there are any collisions with any of the targets
+  for(var i=0;i<targets.length;i++){
+    if(character.collidesWith(targets[i])){
+      // move target out of view and remove it from the array
+      //targets[i].x = 0;
+      //targets[i].y = 0;
+      myStage.removeChild(targets[i]);
+      targets.splice(i, 1);
+      score++;
+      scoreDisplay.text = score;
+      playEatSounds();
+    }
+  }
+}
+
+
+function handlePerimeter(){
+  // check distance from center of the circle, if too big reset to center
+  var x = (stageWidth/2) - character.x;
+  var y = (stageHeight/2) - character.y;
+  var z = Math.sqrt( x*x + y*y );
+  if (z > 415){
+    character.x = stageWidth/2;
+    character.y = stageHeight/2;
+    createjs.Sound.play("edgeSound");
+
+    // set score to zero unless end of game
+    if(theEnd.arrived != true){
+      score = 0;
+      scoreDisplay.text = score;
+    }
+  }
+}
+
+
+
+function playEatSounds(){  
+    var i = Math.floor((Math.random()*5));
+    var x; 
+    switch(i){
+      case 0:
+        x="eatSound1";
+        break;
+      case 1:
+        x="eatSound2";
+        break;
+      case 2:
+        x="eatSound3";
+        break;
+      case 3:
+        x="eatSound4";
+        break;
+      case 4:
+        x="eatSound5";
+        break;
+    }
+  createjs.Sound.play(x);  // Play the sound that corresponds to the random number!
+}
+
+function endCheck(){
+  // if all targets are gone, display end text and don't allow score to reset
+  if(targets.length == 0){
+    theEnd.arrived = true;
+    theEnd.alpha = 1.0;
+  }
+  if (theEnd.arrived == true && endSoundPlayed == false){
+    createjs.Sound.play("endSound");
+    endSoundPlayed = true;
+  }
+}
