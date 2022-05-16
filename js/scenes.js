bdcde90cbc83f8b401544425e2fbc70d30aaafb2
Glitch (glitch-hello-website) <none>
2022-05-16 18:31:58 +0000

    🐧👑 Checkpoint ./js/scenes.js:43384911/330 ./js/script.js:43384911/140

--- js/scenes.js
+++ js/scenes.js
@@ -18,7 +18,7 @@ var scenes = [
    images: [
      {id: "Background", img:"images/MalakaiRoom.png", loc: {x:0, y:0}, animated: false, clickable: false},
      {id: "Malakai Base", img:"images/MalakaiSpriteBase.png", loc: {x:0, y:0}, clickable: false},
-     // {id: "Text Box", img:"images/MalakaiTextBox.png", loc: {x:0, y:0}, animated: false, clickable: false},
+     {id: "Text Box", img:"images/MalakaiTextBox.png", loc: {x:0, y:0}, animated: false, clickable: false},
      ],
    
    actions: [
@@ -45,10 +45,10 @@ var scenes = [
                               endPosition:{x:0, y:0, alpha:1, rotation:0, scale:1},
                               duration: 3000},
                   clickable: false, doNext: "nextAction"},
-     {type: "text", speaker: "Malakai", text: "Hey there, sweetheart. Davie told me there was a new bellhop in town; didn’t expect ya’ to be so damn cute, though.", 
-            loc: {x:800,y:200}, trigger: "stageClick", duration: 3000, doNext: "nextAction", style: whiteTextStyle}]}]
-//      {type: "text", speaker: "Bellhop", text: "The slingshot is the gravity \rof the sun.", 
-//             loc: {x:550,y:520}, trigger: "stageClick", doNext: "nextAction", style: blackTextStyle},
+     {type: "text", speaker: "Malakai", text: "Hey there, sweetheart. Davie told me there was a new bellhop in town; didn’t expect ya’ to be so damn cute, though. What can I do for you?", 
+            loc: {x:800,y:200}, trigger: "stageClick", duration: 3000, doNext: "nextAction", style: whiteTextStyle},
+     {type: "text", speaker: "Bellhop", text: "The slingshot is the gravity \rof the sun.", 
+            loc: {x:550,y:520}, trigger: "stageClick", doNext: "nextAction", style: blackTextStyle},
 //      {type: "text", speaker: "Villain", text: "The stone is I, and you are the target.", 
 //             loc: {x:800,y:200}, trigger: "stageClick", doNext: "nextAction", style: whiteTextStyle},
 //      // {type: "text", speaker: "Villain", text: "There is no attraction, \ras the force of our common star-\ra body so colossal it cannot \rbe called 'object'...", 
@@ -60,12 +60,12 @@ var scenes = [
 //                               startPosition:{scale:1},
 //                               endPosition:{x:780, y: 600, scale:1.4},
 //                               duration: 3000}, trigger: "stageClick", duration: 4000, doNext: "nextAction"},
-//      {type: "text", speaker: "Choice", text: "Take the door on the left", loc: {x:950,y:620}, 
-//             trigger: "choice", doNext: "scene2a", hideAfter:2},
-//      {type: "text", speaker: "Choice", text: "Take the door on the right", loc: {x:950,y:690}, 
-//             trigger: "lastchoice", doNext: "scene2b"}]},
+     {type: "text", speaker: "Bellhop", text: "Oh, uhm. I’m just checking in; Mr. Jonas told me to come meet the regulars? Are regulars a thing at hotels?", loc: {x:950,y:620}, 
+            trigger: "choice", doNext: "MalakaiResponse1", hideAfter:2},
+     {type: "text", speaker: "Bellhop", text: "Take the door on the right", loc: {x:950,y:690}, 
+            trigger: "lastchoice", doNext: "MalakaiResponse2"}]}]
   
-//    {id:"Elevator", 
+//    {id:"MalakaiResponse1", 
 //     sound: {src: "ElevatorAmbience.mp3", volume: 1.0, loop: -1},
 //     images: [
 //      {id: "Background", img:"images/Elevator.png", loc: {x:0, y:0}, animated: false, clickable: false},
